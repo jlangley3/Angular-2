d3c4c850a45d81d7d6f8fe51fca474d2c5f8c119
jlangley3 <jlangleyiii@aol.com>
2020-10-31 12:19:05 -0400

    added interfaces'

---
+++ src/app/shared/interfaces.ts
@@ -0,0 +1,19 @@
+
+export interface ICustomer {
+    id: number;
+    name: string;
+    city: string;
+    orderTotal?: number;
+    customerSince: any;
+}
+
+export interface IOrder {
+    customerId: number;
+    orderItems: IOrderItem[];
+}
+
+export interface IOrderItem {
+    id: number;
+    productName: string;
+    itemCost: number;
+}
