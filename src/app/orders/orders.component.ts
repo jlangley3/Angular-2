87bad85b1f003cb28e4fae97fe4c3d3232646099
jlangley3 <jlangleyiii@aol.com>
2020-10-31 23:09:35 -0400

    adding more routes

--- src/app/orders/orders.component.ts
+++ src/app/orders/orders.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+import { DataService } from '../core/data.service';
+import { ICustomer, IOrder, IOrderItem } from '../shared/interfaces';
 
 @Component({
   selector: 'app-orders',
   templateUrl: './orders.component.html',
-  styleUrls: ['./orders.component.css']
+  styleUrls: [ './orders.component.css' ]
 })
 export class OrdersComponent implements OnInit {
 
-  constructor() { }
+  orders: IOrder[] = [];
+  customer: ICustomer;
 
-  ngOnInit(): void {
+  constructor(private dataService: DataService, 
+              private route: ActivatedRoute) { }
+
+  ngOnInit() {
+
+
+
+    this.dataService.getCustomer(id).subscribe((customer: ICustomer) => {
+      this.customer = customer;
+    });
   }
 
 }
